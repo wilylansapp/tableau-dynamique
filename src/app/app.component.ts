c117257c7a79f8bc813865322f3dca3eb10d68fe
wilylansapp <71969380+wilylansapp@users.noreply.github.com>
2023-07-29 10:56:53 +0200

    update table add column

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'my-app',
@@ -9,12 +10,34 @@ export class AppComponent {
   public data = [
     { name: 'arazr', number: 1 },
     { name: 'rzrt', number: 2 },
+    { name: 'pakd', number: 2 },
   ];
-  public transposeView = false;
+  public transposeView = true;
+  public columns = [];
   private columnCounter = 0;
+
+  constructor() {
+    this.columns = this.getColumns();
+  }
+
+  getColumns() {
+    const columns = [];
+    const keys = Object.keys(this.data[0]);
+    keys.forEach((key, index) => {
+      const column = [];
+      column.push(key);
+      this.data.forEach((row) => column.push(row[key]));
+      columns.push(column);
+    });
+    return columns;
+  }
 
   objectKeys(row: any) {
     return Object.keys(row);
+  }
+
+  objectValues(row: any) {
+    return Object.values(row);
   }
 
   switchView() {
@@ -24,11 +47,17 @@ export class AppComponent {
   addColumn() {
     const newColumnKey = 'newColumn' + this.columnCounter++;
     this.data.forEach((row) => (row[newColumnKey] = Math.random()));
+    this.columns = this.getColumns();
   }
 
   addRow() {
     const newRow = {};
     Object.keys(this.data[0]).forEach((key) => (newRow[key] = Math.random()));
     this.data.push(newRow);
+    this.columns = this.getColumns();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { GridModule } from '@syncfusion/ej2-angular-grids';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,GridModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, GridModule, DragDropModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
