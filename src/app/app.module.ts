9b7cd267d03d4afdcbcc6ed35cbd929657bc1858
wilylansapp <71969380+wilylansapp@users.noreply.github.com>
2023-07-30 13:28:46 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { GridModule } from '@syncfusion/ej2-angular-grids';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, GridModule, DragDropModule],
+  imports: [BrowserModule, FormsModule, DragDropModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
